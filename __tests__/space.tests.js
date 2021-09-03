a72077a6e86775935f388453f17226455c5cd851
chynnalew <chynnalew@yahoo.com>
2021-09-03 09:11:04 -0700

    pass test 1: create user object

--- __tests__/space.tests.js
+++ __tests__/space.tests.js
@@ -3,10 +3,15 @@ import {UserAge} from './../src/userAge';
 describe ('UserAge', () => {
   let user;
   beforeEach(() => {
-    user = new UserAge("Mario");
+    user = new UserAge("Mario", 40);
   });
 
   test('should create a new user object using the UserAge constructor', () => {
     expect(user.name).toEqual("Mario");
+    expect(user.earth).toEqual(40);
+    expect(user.mercury).toEqual(0);
+    expect(user.venus).toEqual(0);
+    expect(user.mars).toEqual(0);
+    expect(user.jupiter).toEqual(0);
   });
 })
--- src/userAge.js
+++ src/userAge.js
@@ -1,3 +1,10 @@
 export class UserAge {
-
+  constructor(name, earth) {
+    this.name = name;
+    this.earth = earth;
+    this.mercury = 0;
+    this.venus = 0;
+    this.mars = 0;
+    this.jupiter = 0;
+  }
 }
