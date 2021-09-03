42c2b0b0be460a77157ca0ce2ccd2612cd99f9f8
chynnalew <chynnalew@yahoo.com>
2021-09-03 11:34:30 -0700

    good fail test7: calculate mercury time left

--- __tests__/space.tests.js
+++ __tests__/space.tests.js
@@ -3,13 +3,12 @@ import {UserAge, UserLife} from './../src/userAge';
 describe ('UserAge', () => {
   let user;
   beforeEach(() => {
-    user = new UserLife("Mario", 40, 78.56);
+    user = new UserAge("Mario", 40);
   });
 
   test('should create a new user object using the UserAge constructor', () => {
     expect(user.name).toEqual("Mario");
     expect(user.earth).toEqual(40);
-    expect(user.earthExp).toEqual(78.56)
   });
   test('should add the calculated mercury age to user.mercury', () => {
     expect(user.mercury).toBeCloseTo(166.66, 1);
@@ -23,10 +22,20 @@ describe ('UserAge', () => {
   test('should add the calculated jupiter age to user.jupiter', () => {
     expect(user.jupiter).toBeCloseTo(3.37, 1);
   });
+});
+
+describe('UserLife', () => {
+  let user;
+  beforeEach(() => {
+    user = new UserLife("Mario", 40, 78.56);
+  });
   test('should calculate life expectancy on all planets', () => {
     expect(user.mercuryExp).toBeCloseTo(327.33, 1);
     expect(user.venusExp).toBeCloseTo(126.71, 1);
     expect(user.marsExp).toBeCloseTo(41.78, 1);
     expect(user.jupiterExp).toBeCloseTo(6.62, 1);
   });
-});
+  test('should calculate how many years the user has left on mercury', () => {
+    expect(user.mercuryTime()).toBeCloseTo(160.67, 1);
+  })
+})
--- src/userAge.js
+++ src/userAge.js
@@ -6,8 +6,6 @@ export class UserAge {
     this.venus = (this.earth / 0.62);
     this.mars = (this.earth / 1.88);
     this.jupiter = (this.earth / 11.86);
-
-   
   }
 }
 
@@ -19,6 +17,10 @@ export class UserLife extends UserAge {
     this.venusExp = (this.earthExp / 0.62);
     this.marsExp = (this.earthExp / 1.88);
     this.jupiterExp = (this.earthExp / 11.86);
+  }
+
+  mercuryTime() {
+    
   }
 }
 
