5fc4aeabf200a39f3fda54e8eb668f7f65cab48c
chynnalew <chynnalew@yahoo.com>
2021-09-03 13:28:56 -0700

    add UI to create user and  populate table

--- src/index.js
+++ src/index.js
@@ -5,16 +5,29 @@ import './css/styles.css';
 import {UserLife} from './userAge.js';
 
 $(document).ready(function() {
-  $('#blastOff').on('click', function(event) {
+  $('#form').submit(function(event) {
     event.preventDefault();
+    console.log("hi");
     const name = $('#inputName').val();
     const age = $('#inputAge').val();
     const lifeExp = $('#inputLife').val();
     const user = new UserLife(name,age,lifeExp);
     $('#earthAge').html(user.earth);
     $('#earthLife').html(user.earthExp);
-    $('#earthLeft').html(user.earthTime())
-  })
+    $('#earthLeft').html(user.earthTime());
+    $('#mercuryAge').html(user.mercury.toFixed(2));
+    $('#mercuryLife').html(user.mercuryExp.toFixed(2));
+    $('#mercuryLeft').html(user.mercuryTime().toFixed(2));
+    $('#venusAge').html(user.venus.toFixed(2));
+    $('#venusLife').html(user.venusExp.toFixed(2));
+    $('#venusLeft').html(user.venusTime().toFixed(2));
+    $('#marsAge').html(user.mars.toFixed(2));
+    $('#marsLife').html(user.marsExp.toFixed(2));
+    $('#marsLeft').html(user.marsTime().toFixed(2));
+    $('#jupiterAge').html(user.jupiter.toFixed(2));
+    $('#jupiterLife').html(user.jupiterExp.toFixed(2));
+    $('#jupiterLeft').html(user.jupiterTime().toFixed(2));
   });
+});
 
   
